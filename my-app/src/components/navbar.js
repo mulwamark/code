e1c4284d436086afbc10459be1f704332d381506
mulwamark <mulwamark@gmail.com>
2023-10-03 23:06:00 +0300

    creating of navbar component

--- my-app/src/components/navbar.js
+++ my-app/src/components/navbar.js
@@ -1,5 +1,10 @@
 import React from 'react';
 
 export default function navbar(){
-    return <hi>navbar component</hi>
+    return (
+        <navbar>
+             <img src='./image/logo.png'/>
+            <h3>React</h3>
+            <h4>React course projrct</h4>
+        </navbar>)
 }
