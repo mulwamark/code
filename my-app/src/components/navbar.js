70976078bbb3a256c5b9400906617610809c4646
mulwamark <mulwamark@gmail.com>
2023-10-07 12:40:50 +0300

    modifying of navbar image

--- my-app/src/components/navbar.js
+++ my-app/src/components/navbar.js
@@ -1,9 +1,13 @@
 import React from 'react';
+import logo from '../image/logo.png'
 
 export default function navbar(){
     return (
         <navbar>
-             <img src='./image/logo.png'/>
+             <img 
+             src={logo}
+             alt='logo.png'
+             className='nav--icon'/>
             <h3>React</h3>
             <h4>React course projrct</h4>
         </navbar>)
