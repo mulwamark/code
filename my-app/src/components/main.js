358b16c29101b9e13b111ef337e43bf05aa4c14a
mulwamark <mulwamark@gmail.com>
2023-10-10 21:46:15 +0300

    styling main body

--- my-app/src/components/main.js
+++ my-app/src/components/main.js
@@ -2,8 +2,8 @@ import React from 'react';
 
 export default function main(){
     return <main>
-        <h1>FUN FACTS ABOUT REACTS</h1>
-        <ul>
+        <h1 className='main--title'>FUN FACTS ABOUT REACTS</h1>
+        <ul className='main--facts'>
             <li>Was first realesd on 2013</li>
             <li>was first created by jordan mark</li>
             <li>has over 100k stars on github</li>
